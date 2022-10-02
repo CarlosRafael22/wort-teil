e4bbf254bb80aac15183fe2e8dbb4da96e21dbf9
Rafael Leitão <carlosrafael093@gmail.com>
2022-10-02 19:56:19 +0200

    feat(utils): Create isVerb predicate check

--- src/constants/utils.ts
+++ src/constants/utils.ts
@@ -1,7 +1,11 @@
-import {NounType, AdjectiveType} from './types';
+import {NounType, AdjectiveType, VerbType} from './types';
 
 type WordType = NounType | AdjectiveType;
 
 export const isNoun = (word: WordType): word is NounType => {
   return !!(word as NounType).article;
+};
+
+export const isVerb = (word: WordType): word is VerbType => {
+  return !!(word as VerbType).example;
 };
