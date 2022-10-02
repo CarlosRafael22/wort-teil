2c03131d299edc025f866b4e39f15fcf46b077d1
Rafael Leitão <carlosrafael093@gmail.com>
2022-10-02 19:25:51 +0200

    feat(constants): Create adjective file

---
+++ src/constants/adjective.ts
@@ -0,0 +1,50 @@
+import {AdjectiveType} from './types';
+
+const FOOD_RELATED: AdjectiveType[] = [
+  {
+    word: 'Heiß',
+    translation: 'Hot',
+  },
+  {
+    word: 'Kalt',
+    translation: 'Cold',
+  },
+  {
+    word: 'Hart',
+    translation: 'Hard',
+  },
+  {
+    word: 'Eklig',
+    translation: 'Disgusting',
+  },
+  {
+    word: 'Lecker',
+    translation: 'Tasty',
+  },
+  {
+    word: 'Saftig',
+    translation: 'Juicy',
+  },
+  {
+    word: 'Salzig',
+    translation: 'Salty',
+  },
+  {
+    word: 'Sauer',
+    translation: 'Sour',
+  },
+  {
+    word: 'Scharf',
+    translation: 'Spicy',
+  },
+  {
+    word: 'Süß',
+    translation: 'Sweet',
+  },
+  {
+    word: 'Trocken',
+    translation: 'Dry',
+  },
+];
+
+export const ADJECTIVES = [...FOOD_RELATED];
