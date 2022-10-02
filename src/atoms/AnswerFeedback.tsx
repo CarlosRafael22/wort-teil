bd14d184d30e4b4e22c4cc0324922a317ce32cbb
Rafael Leitão <carlosrafael093@gmail.com>
2022-10-02 11:16:53 +0200

    feat(atoms): Create AnswerFeedback

---
+++ src/atoms/AnswerFeedback.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+import Icon from 'react-native-vector-icons/Entypo';
+
+interface Props {
+  answeredCorrectly: boolean;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: 80,
+    height: 80,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 40,
+    marginTop: 40,
+  },
+});
+
+const AnswerFeedback = ({answeredCorrectly}: Props) => {
+  return (
+    <View
+      style={[
+        styles.container,
+        {backgroundColor: answeredCorrectly ? 'green' : 'red'},
+      ]}>
+      {answeredCorrectly ? (
+        <Icon name="check" size={30} />
+      ) : (
+        <Icon name="cross" size={30} />
+      )}
+    </View>
+  );
+};
+
+export default AnswerFeedback;
