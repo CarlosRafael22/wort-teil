9bbefa8118f721f95d1ef17ba3c4e6be9f368b3f
Rafael Leitão <carlosrafael093@gmail.com>
2022-10-02 11:26:46 +0200

    refactor(App): Delete unused code from template

--- App.tsx
+++ App.tsx
@@ -1,77 +1,25 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React, {useState} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {SafeAreaView, ScrollView, StatusBar, View} from 'react-native';
 
 import NounPage from './src/pages/NounPage';
-import {NounType} from './src/constants/types';
 import NOUNS from './src/constants/noun';
 
-const Section = ({children, title}): React.ReactElement => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const getRandomNumber = max => {
+const getRandomNumber = (max: number) => {
   return Math.floor(Math.random() * max);
 };
 
 const App: () => React.ReactElement = () => {
-  const isDarkMode = useColorScheme() === 'dark';
   const [currentNounIndex, setCurrentNounIndex] = useState(0);
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
 
   const goToNextPage = () => {
     setTimeout(() => {
-      console.log('GO TO NEXT');
       const page = getRandomNumber(NOUNS.length);
       const nextPage = page === NOUNS.length ? 0 : page;
       setCurrentNounIndex(nextPage);
-    }, 1500);
+    }, 1000);
   };
 
   const handleAnswer = (answer: string) => {
-    console.log('ANSWERED PAGE: ', answer);
     if (answer === NOUNS[currentNounIndex].article) {
       goToNextPage();
     } else {
@@ -79,17 +27,11 @@ const App: () => React.ReactElement = () => {
     }
   };
 
-  console.log('APP: ', NOUNS[currentNounIndex].word);
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
+    <SafeAreaView>
+      <StatusBar />
+      <ScrollView contentInsetAdjustmentBehavior="automatic">
+        <View>
           <NounPage
             noun={NOUNS[currentNounIndex]}
             handleAnswer={handleAnswer}
@@ -99,24 +41,5 @@ const App: () => React.ReactElement = () => {
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
