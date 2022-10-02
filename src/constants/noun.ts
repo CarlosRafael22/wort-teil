30b50b5375d8853818c6a28defcd14d3fba5ff4d
Rafael Leitão <carlosrafael093@gmail.com>
2022-10-02 09:27:25 +0200

    feat(constants): Create noun file

---
+++ src/constants/noun.ts
@@ -0,0 +1,134 @@
+import {NounType} from './types';
+
+const OBST: NounType[] = [
+  {
+    article: 'Der',
+    word: 'Apfel',
+    translation: 'Apple',
+  },
+  {
+    article: 'Die',
+    word: 'Erdbeere',
+    translation: 'Strawberry',
+  },
+  {
+    article: 'Die',
+    word: 'Traube',
+    translation: 'Grapefruit',
+  },
+  {
+    article: 'Die',
+    word: 'Orange',
+    translation: 'Orange',
+  },
+  {
+    article: 'Die',
+    word: 'Orange',
+    translation: 'Orange',
+  },
+  {
+    article: 'Die',
+    word: 'Banane',
+    translation: 'Banana',
+  },
+  {
+    article: 'Die',
+    word: 'Ananas',
+    translation: 'Pineapple',
+  },
+  {
+    article: 'Die',
+    word: 'Zitrone',
+    translation: 'Lemon',
+  },
+];
+
+const KLEIDUNG: NounType[] = [
+  {
+    article: 'Der',
+    word: 'Anzug',
+    translation: 'Suit',
+  },
+  {
+    article: 'Die',
+    word: 'Bluse',
+    translation: 'Blouse',
+  },
+  {
+    article: 'Das',
+    word: 'Hemd',
+    translation: 'Shirt',
+  },
+  {
+    article: 'Die',
+    word: 'Hose',
+    translation: 'Pants',
+  },
+  {
+    article: 'Der',
+    word: 'Rock',
+    translation: 'Skirt',
+  },
+  {
+    article: 'Der',
+    word: 'Schuh',
+    translation: 'Shoe',
+  },
+  {
+    article: 'Der',
+    word: 'Stiefel',
+    translation: 'Boot',
+  },
+  {
+    article: 'Der',
+    word: 'Handschuh',
+    translation: 'Glove',
+  },
+];
+
+const MOBEL: NounType[] = [
+  {
+    article: 'Das',
+    word: 'Bett',
+    translation: 'Bed',
+  },
+  {
+    article: 'Das',
+    word: 'Sofa',
+    translation: 'Sofa',
+  },
+  {
+    article: 'Das',
+    word: 'Wandregal',
+    translation: 'Shelf',
+  },
+  {
+    article: 'Der',
+    word: 'Hocker',
+    translation: 'Stool',
+  },
+  {
+    article: 'Der',
+    word: 'Nachttisch',
+    translation: 'Nightstand',
+  },
+  {
+    article: 'Der',
+    word: 'Spiegel',
+    translation: 'Mirror',
+  },
+  {
+    article: 'Der',
+    word: 'Tisch',
+    translation: 'Table',
+  },
+  {
+    article: 'Der',
+    word: 'Wäschkorb',
+    translation: 'Laundry basket',
+  },
+];
+
+const NOUNS = [...OBST, ...KLEIDUNG, ...MOBEL];
+
+export default NOUNS;
