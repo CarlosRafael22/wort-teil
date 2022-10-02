efea3af475faf24db34511ba8552aabc632b4328
Rafael Leitão <carlosrafael093@gmail.com>
2022-10-02 19:27:33 +0200

    feat(App): Add AdjectivePage logic

--- App.tsx
+++ App.tsx
@@ -4,26 +4,49 @@ import {SafeAreaView, ScrollView, StatusBar, View} from 'react-native';
 import NounPage from './src/pages/NounPage';
 import NOUNS from './src/constants/noun';
 
+import AdjectivePage from './src/pages/AdjectivePage';
+import {ADJECTIVES} from './src/constants/adjective';
+import {AdjectiveType, NounType} from './src/constants/types';
+import {isNoun} from './src/constants/utils';
+
+const PAGE_TYPES = {
+  NOUN: 'NOUN',
+  ADJECTIVE: 'ADJECTIVE',
+};
+
 const getRandomNumber = (max: number) => {
   return Math.floor(Math.random() * max);
 };
 
 const App: () => React.ReactElement = () => {
-  const [currentNounIndex, setCurrentNounIndex] = useState(0);
+  const [page, setPage] = useState({
+    pageIndex: 0,
+    pageType: PAGE_TYPES.NOUN,
+  });
+
+  const WORDS: (AdjectiveType | NounType)[] = [...NOUNS, ...ADJECTIVES];
 
   const goToNextPage = () => {
-    setTimeout(() => {
-      const page = getRandomNumber(NOUNS.length);
-      const nextPage = page === NOUNS.length ? 0 : page;
-      setCurrentNounIndex(nextPage);
-    }, 1000);
+    const delay = page.pageType === PAGE_TYPES.NOUN ? 1000 : 500;
+
+    const timeoutId = setTimeout(() => {
+      clearTimeout(timeoutId);
+      const pageNumber = getRandomNumber(WORDS.length);
+      const nextPageIndex = pageNumber === WORDS.length ? 0 : pageNumber;
+
+      const nextWord = WORDS[nextPageIndex];
+      const nextPage = {
+        pageIndex: nextPageIndex,
+        pageType: isNoun(nextWord) ? PAGE_TYPES.NOUN : PAGE_TYPES.ADJECTIVE,
+      };
+
+      setPage(nextPage);
+    }, delay);
   };
 
   const handleAnswer = (answer: string) => {
-    if (answer === NOUNS[currentNounIndex].article) {
+    if (answer === (WORDS[page.pageIndex] as NounType).article) {
       goToNextPage();
-    } else {
-      setCurrentNounIndex(currentNounIndex);
     }
   };
 
@@ -32,10 +55,18 @@ const App: () => React.ReactElement = () => {
       <StatusBar />
       <ScrollView contentInsetAdjustmentBehavior="automatic">
         <View>
-          <NounPage
-            noun={NOUNS[currentNounIndex]}
-            handleAnswer={handleAnswer}
-          />
+          {page.pageType === PAGE_TYPES.NOUN && (
+            <NounPage
+              noun={WORDS[page.pageIndex] as NounType}
+              handleAnswer={handleAnswer}
+            />
+          )}
+          {page.pageType === PAGE_TYPES.ADJECTIVE && (
+            <AdjectivePage
+              adjective={WORDS[page.pageIndex] as AdjectiveType}
+              onPressNext={goToNextPage}
+            />
+          )}
         </View>
       </ScrollView>
     </SafeAreaView>
