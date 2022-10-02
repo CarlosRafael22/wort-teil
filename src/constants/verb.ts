216d4c61d5cb1eb8e6276a652e5687240ca64072
Rafael Leitão <carlosrafael093@gmail.com>
2022-10-02 19:55:40 +0200

    feat(constants): Create verb file

---
+++ src/constants/verb.ts
@@ -0,0 +1,68 @@
+import {VerbType} from './types';
+
+const VERBS_1: VerbType[] = [
+  {
+    word: 'Besuchen',
+    translation: 'Visit',
+    example: 'Wir besuchen meine Tante in Berlin.',
+  },
+  {
+    word: 'Bestellen',
+    translation: 'Order',
+    example: 'Er bestellt es online',
+  },
+  {
+    word: 'Nehmen',
+    translation: 'Take',
+    example: 'Nehmt ihr das Geld?',
+  },
+  {
+    word: 'Schmecken',
+    translation: 'Taste',
+    example: 'Das Eis schmeckt total lecker.',
+  },
+  {
+    word: 'Vergessen',
+    translation: 'Forget',
+    example: 'Ich vergesse den Namen.',
+  },
+  {
+    word: 'Zeigen',
+    translation: 'Show',
+    example: 'Ich zeige Ihnen, wo das ist.',
+  },
+  {
+    word: 'Suchen',
+    translation: 'Search',
+    example: 'Was suchst du?',
+  },
+  {
+    word: 'Warten',
+    translation: 'Wait',
+    example: 'Sie wartet auf den Bus.',
+  },
+  {
+    word: 'Kommen',
+    translation: 'Come',
+    example: 'Ich komme heute etwas später nach Hause.',
+  },
+  {
+    word: 'Wollen',
+    translation: 'Want',
+    example: 'Willst du heute Abend einen Film sehen?',
+  },
+  {
+    word: 'Machen',
+    translation: 'Make',
+    example: 'Ich mache jeden Tag Frühstück.',
+  },
+  {
+    word: 'Bleiben',
+    translation: 'Stay',
+    example: 'Bleibst du noch auf einen Kaffee?',
+  },
+];
+
+const VERBS = [...VERBS_1];
+
+export default VERBS;
