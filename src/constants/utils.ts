b176fdf7d4f6d4925a88771d4b97df54a007ac9b
Rafael Leitão <carlosrafael093@gmail.com>
2022-10-11 21:11:20 +0200

    feat(utils): Add getType and PAGE_TYPES

--- src/constants/utils.ts
+++ src/constants/utils.ts
@@ -1,11 +1,46 @@
-import {NounType, AdjectiveType, VerbType} from './types';
+import {
+  NounType,
+  AdjectiveType,
+  VerbType,
+  SentenceType,
+  DataType,
+} from './types';
 
-type WordType = NounType | AdjectiveType;
+export const PAGE_TYPES = {
+  NOUN: 'NOUN',
+  ADJECTIVE: 'ADJECTIVE',
+  VERB: 'VERB',
+  SENTENCE: 'SENTENCE',
+} as const;
 
-export const isNoun = (word: WordType): word is NounType => {
+export const isNoun = (word: DataType): word is NounType => {
   return !!(word as NounType).article;
 };
 
-export const isVerb = (word: WordType): word is VerbType => {
+export const isVerb = (word: DataType): word is VerbType => {
   return !!(word as VerbType).example;
+};
+
+export const isSentence = (word: DataType): word is SentenceType => {
+  return !!(word as SentenceType).sentence;
+};
+
+export const isAdjective = (word: DataType): word is AdjectiveType => {
+  return (
+    !!(word as AdjectiveType).word &&
+    !(word as NounType).article &&
+    !(word as VerbType).example
+  );
+};
+
+export const getType = (word: DataType): keyof typeof PAGE_TYPES => {
+  if (isNoun(word)) {
+    return PAGE_TYPES.NOUN;
+  } else if (isAdjective(word)) {
+    return PAGE_TYPES.ADJECTIVE;
+  } else if (isVerb(word)) {
+    return PAGE_TYPES.VERB;
+  } else {
+    return PAGE_TYPES.SENTENCE;
+  }
 };
