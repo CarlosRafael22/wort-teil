af6e499555125c4e2adf4daf17779622a44911f3
Rafael Leitão <carlosrafael093@gmail.com>
2022-10-11 21:12:27 +0200

    feat(constants): Create sentences file

---
+++ src/constants/sentences.ts
@@ -0,0 +1,13 @@
+const OBST = [
+  {
+    sentence: 'Der Apfel ist sehr süß.',
+    translatedSentence: 'The apple is very sweet.',
+  },
+  {
+    sentence: 'Die Banane ist Gelbe.',
+    translatedSentence: 'The banana is yellow.',
+  },
+];
+
+const SENTENCES = [...OBST];
+export default SENTENCES;
