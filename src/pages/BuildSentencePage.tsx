f41f6652ff07b8a77bf6c690fa02a20892c01d3f
Rafael Leitão <carlosrafael093@gmail.com>
2022-10-11 21:16:03 +0200

    feat(pages): Create BuildSentencePage

---
+++ src/pages/BuildSentencePage.tsx
@@ -0,0 +1,132 @@
+import React, {useState} from 'react';
+import {View, StyleSheet, TextInput, Text} from 'react-native';
+
+import {SentenceType} from '../constants/types';
+import Button from '../atoms/Button';
+import AnswerFeedback from '../atoms/AnswerFeedback';
+
+interface Props {
+  sentence: SentenceType;
+  onPressNext: () => void;
+}
+
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: 'green',
+  },
+  container: {
+    display: 'flex',
+    backgroundColor: '#e5e5e5',
+    flexGrow: 1,
+  },
+  body: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    flexGrow: 1,
+  },
+  content: {
+    backgroundColor: 'yellow',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  feedback: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'violet',
+    height: 100,
+  },
+  footer: {
+    backgroundColor: 'pink',
+    paddingTop: 16,
+    paddingBottom: 16,
+    paddingLeft: 8,
+    paddingRight: 8,
+  },
+  input: {
+    height: 40,
+    width: '80%',
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    fontSize: 18,
+  },
+  translatedSentence: {
+    fontSize: 28,
+    fontWeight: '700',
+  },
+});
+
+const BUTTON_TEXT = {
+  CHECK: 'Check',
+  NEXT: 'Next',
+};
+
+const BuildSentencePage = ({
+  sentence: sentenceObj,
+  onPressNext,
+}: Props): React.ReactElement => {
+  const [answeredCorrectly, setAnsweredCorrectly] = useState<boolean | null>(
+    null,
+  );
+  const [typedSentence, setTypedSentence] = useState('');
+  const [buttonText, setButtonText] = useState(BUTTON_TEXT.CHECK);
+
+  const {sentence, translatedSentence} = sentenceObj;
+
+  const handlePress = () => {
+    if (buttonText === BUTTON_TEXT.NEXT) {
+      onPressNext();
+    } else {
+      if (typedSentence === sentence) {
+        setAnsweredCorrectly(true);
+        setButtonText(BUTTON_TEXT.NEXT);
+      } else {
+        setAnsweredCorrectly(false);
+        setTimeout(() => {
+          setAnsweredCorrectly(null);
+        }, 1000);
+      }
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.translatedSentence}>Translate this sentence</Text>
+      </View>
+      <View style={styles.body}>
+        <View style={styles.content}>
+          <Text style={styles.translatedSentence}>{translatedSentence}</Text>
+          <TextInput
+            style={styles.input}
+            value={typedSentence}
+            onChangeText={setTypedSentence}
+          />
+        </View>
+
+        <View style={styles.feedback}>
+          {answeredCorrectly !== null && (
+            <AnswerFeedback answeredCorrectly={answeredCorrectly} />
+          )}
+        </View>
+      </View>
+
+      <View style={styles.footer}>
+        <Button
+          text={buttonText}
+          onPress={handlePress}
+          styles={{
+            buttonStyle: {
+              backgroundColor:
+                buttonText === BUTTON_TEXT.CHECK ? '#00BFFF' : '#3CB371',
+            },
+          }}
+        />
+      </View>
+    </View>
+  );
+};
+
+export default BuildSentencePage;
