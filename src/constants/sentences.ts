57755e116d09a411cc018780c20f130548a6d546
Rafael Leitão <carlosrafael093@gmail.com>
2022-10-23 00:24:16 +0200

    feat(sentences): Add more sentences

--- src/constants/sentences.ts
+++ src/constants/sentences.ts
@@ -9,5 +9,48 @@ const OBST = [
   },
 ];
 
-const SENTENCES = [...OBST];
+const MOBEL = [
+  {
+    sentence: 'Die Kommode ist klein aber sehr schön.',
+    translatedSentence: 'The dresser is small but very beautiful.',
+  },
+  {
+    sentence: 'Der Schrank ist groß aber ein bisschein hässlich.',
+    translatedSentence: 'The wardrobe is big but a little ugly.',
+  },
+  {
+    sentence: 'Das Bett ist neu und schön.',
+    translatedSentence: 'The bed is new and beautiful.',
+  },
+  {
+    sentence: 'Die Spüle ist zu klein für die Küche.',
+    translatedSentence: 'The sink is too small for the kitchen.',
+  },
+  {
+    sentence: 'Der Fernsehen ist zu größ für das Wohnzimmer.',
+    translatedSentence: 'The tv is too big for the living room.',
+  },
+  {
+    sentence: 'Das Sofa ist sehr alt und hässlich.',
+    translatedSentence: 'The sofa is really old and ugly.',
+  },
+  {
+    sentence: 'Der Kühlschrank ist neu und modern.',
+    translatedSentence: 'The fridge is new and modern.',
+  },
+  {
+    sentence: 'Das Regal ist Gelbe.',
+    translatedSentence: 'The shelf is yellow.',
+  },
+  {
+    sentence: 'Der Herd ist modern.',
+    translatedSentence: 'The stove is modern.',
+  },
+  {
+    sentence: 'Der Teppich ist sehr größ.',
+    translatedSentence: 'The carpet is very big.',
+  },
+];
+
+const SENTENCES = [...OBST, MOBEL];
 export default SENTENCES;
